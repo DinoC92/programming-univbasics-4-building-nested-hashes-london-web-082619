f47baff18a9167c36bd8ca3eb864476d6ba563cf
Dean Burgess <dinocastiglione@ymail.com>
2019-08-12 18:40:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,7 +46,7 @@ def monopoly_with_fourth_tier
        three_pieces_owned: 100,
        four_pieces_owned: 200
      },
-     :names => {:pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {}, :shortline_railroad => {},:reading_railroad => {"mortgage_value" => "$100"} 
+     :names => {:pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline_railroad => {},:reading_railroad => {"mortgage_value" => "$100"} 
      }
    }
    }
